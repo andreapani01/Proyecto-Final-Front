b6f2fabad33bb314c0a6972ca7da641a1538ea1f
Miguel Navarro <migueln1511@gmail.com>
2024-11-24 21:48:54 -0500

    fix menor

--- src/boot/google-maps.js
+++ src/boot/google-maps.js
@@ -4,7 +4,7 @@ import VueGoogleMaps from "@fawmi/vue-google-maps";
 export default boot(({ app }) => {
   app.use(VueGoogleMaps, {
     load: {
-      key: "AIzaSyCGTlQwb_dpzNvUHK8OOmv7ITio91IluMo",
+      key: "",
     },
   });
 });
